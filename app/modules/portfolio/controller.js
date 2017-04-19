8d625d60ecaccba5bb15333964174a4f09c78fd4
nmajors <nmajors@gmail.com>
2017-04-19 08:04:55 -0500

    fixed event listeners

--- app/modules/portfolio/controller.js
+++ app/modules/portfolio/controller.js
@@ -3,12 +3,9 @@ class PortfolioController {
         this.events = document.querySelectorAll(".entry");
         this.triggerOffset = 0.9;
 
-        this.hoverEls = document.querySelectorAll(".surprise");
-
-        this.eventListeners();
-        // this.rainbow();
+        this.scrollEventListeners();
         this.showBlocks();
-        this.hoverSurprise();
+        this.hoverEventListeners();
       }
 
       offset(el) {
@@ -19,7 +16,7 @@ class PortfolioController {
         };
       }
 
-      eventListeners() {
+      scrollEventListeners() {
         window.addEventListener("scroll", (e) => {
           if (!window.requestAnimationFrame) {
             setTimeout(() => {
@@ -34,8 +31,27 @@ class PortfolioController {
         });
       }
 
+    hoverEventListeners(){
+      document.querySelector(".about--item").addEventListener('mouseover', (evn) => {
+        let pic = document.querySelector(".nancyPhoto");
+        if (evn.target.classList.contains("surprise")){
+         document.body.style.cursor = "pointer";
+         pic.style.backgroundImage = "url('https://blobtoblog.files.wordpress.com/2011/05/zoolander-slashie-award.jpg')";
+         pic.style.backgroundSize = "auto";
+         pic.style.border = "1px solid #fff";
+        }
+      })
+      document.querySelector(".about--item").addEventListener('mouseout', (ev) => {
+        let pic = document.querySelector(".nancyPhoto");
+        if (ev.target.classList.contains("surprise")){
+         document.body.style.cursor = "default";
+         pic.style.backgroundImage = "url('./assets/images/nancy.png')";
+               pic.style.backgroundSize = "cover";
+               pic.style.border = "1px solid black";
+        }
+      });
+    }
     	showBlocks(blocks, offset) {
-
         Array.from(this.events).forEach((event) => {
           if (this.offset(event).top <= (document.body.scrollTop + window.innerHeight * this.triggerOffset)) {
             let img = event.querySelector(".entry--img");
@@ -50,107 +66,6 @@ class PortfolioController {
           }
         });
     	}
-
-
-      hoverSurprise(){
-        let pic = document.querySelector(".nancyPhoto");
-        console.log(this.hoverEls);
-        Array.from(this.hoverEls).forEach((hoverEl)=>{
-          console.log(hoverEl);
-          hoverEl.addEventListener("mouseenter", (ev) =>{
-            console.log("hi");
-            document.body.style.cursor = "pointer";
-            pic.style.backgroundImage = "url('https://blobtoblog.files.wordpress.com/2011/05/zoolander-slashie-award.jpg')";
-          })
-          hoverEl.addEventListener("mouseleave", (evn) =>{
-            console.log("bye");
-            document.body.style.cursor = "default";
-            pic.style.backgroundImage = "url('./assets/images/nancy.png')";
-          });
-          hoverEl.addEventListener("onClick")
-        });
-        // let hover = document.querySelector(".surprise");
-        // let pic = document.querySelector(".nancyPhoto");
-
-      }
-
-    }
-
+}
 
 export default PortfolioController;
-
-
-// class entry {
-//   constructor() {
-//     this.events = document.querySelectorAll(".entry");
-//     this.triggerOffset = 0.9;
-//
-//     this.eventListeners();
-//     // this.rainbow();
-//     this.showBlocks();
-//   }
-//
-//   offset(el) {
-//     let rect = el.getBoundingClientRect(), bodyEl = document.body;
-//
-//     return {
-//       top: rect.top + bodyEl.scrollTop,
-//       left: rect.left + bodyEl.scrollLeft
-//     };
-//   }
-//
-//   eventListeners() {
-//
-//     window.addEventListener("scroll", (e) => {
-//       if (!window.requestAnimationFrame) {
-//         setTimeout(() => {
-//           this.showBlocks();
-//         }, 100);
-//       }
-//       else {
-//         window.requestAnimationFrame(() => {
-//           this.showBlocks();
-//         });
-//       }
-//     });
-//
-//   }
-//
-// 	showBlocks(blocks, offset) {
-//
-//     Array.from(this.events).forEach((event) => {
-//       if (this.offset(event).top <= (document.body.scrollTop + window.innerHeight * this.triggerOffset)) {
-//         let img = event.querySelector(".entry--img");
-//         let content = event.querySelector(".entry--content");
-//
-//         if (img.classList.contains("is-hidden")) {
-//           img.classList.remove("is-hidden");
-//           img.classList.add("bounce-in");
-//           content.classList.remove("is-hidden");
-//           content.classList.add("bounce-in");
-//         }
-//
-//       }
-//     });
-//
-// 	}
-  //
-  // rainbow() {
-  //   let rainbow = new Rainbow();
-  //   rainbow.setNumberRange(0, this.events.length - 1);
-  //   rainbow.setSpectrum("#3498DB", "#BDC3C7");
-  //
-  //   for (var i = 0; i < this.events.length; i++) {
-  //     this.events[i].querySelector(".entry--img").style.backgroundColor = "#" + rainbow.colourAt(i);
-  //
-  //     let tags = this.events[i].querySelectorAll(".tags li");
-  //
-  //     Array.from(tags).forEach((tag) => {
-  //       tag.style.backgroundColor = "#" + rainbow.colourAt(i);
-  //     });
-  //
-  //   }
-  // }
-// }
-//
-// export default entry;
